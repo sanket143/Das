8c86a1427421317b6d24d5b4e519f8cf311a4859
Sanket Chaudhari <sanket@zoho.com>
2018-08-16 08:26:57 +0530

    Added Argument Hadler

--- main.go
+++ main.go
@@ -15,64 +15,79 @@ import (
   "golang.org/x/crypto/ssh/terminal"
 )
 
+func help(){
+  fmt.Println("Das v1.0.1_beta");
+  fmt.Println("usage: das [option]\n");
+  fmt.Println("login : to Log into DANetwork");
+}
+
 func main(){
   // Skip Certificate verification
   http.DefaultTransport.(*http.Transport).TLSClientConfig = &tls.Config{InsecureSkipVerify: true}
 
-  // Regexp Setup
-  ReLogin := regexp.MustCompile("You have successfully logged in");
-  ReMaxLimit := regexp.MustCompile("You have reached Maximum Login Limit");
-  ReInvalidCred := regexp.MustCompile("Make sure your password is correct");
+  args := os.Args[1:];
+  if len(args) == 0 {
+    help();
+    os.Exit(0);
+  } else if args[0] == "login" {
+    // Regexp Setup
+    ReLogin := regexp.MustCompile("You have successfully logged in");
+    ReMaxLimit := regexp.MustCompile("You have reached Maximum Login Limit");
+    ReInvalidCred := regexp.MustCompile("Make sure your password is correct");
 
-  // Reader
-  reader := bufio.NewReader(os.Stdin);
+    // Reader
+    reader := bufio.NewReader(os.Stdin);
 
-  // Getting Creadentials
-getCredentials:
-  fmt.Print("Username: ");
-  inputUsername, _ := reader.ReadString('\n');
+    // Getting Creadentials
+  getCredentials:
+    fmt.Print("Username: ");
+    inputUsername, _ := reader.ReadString('\n');
 
-  fmt.Print("Password: ");
-  bytePassword, err := terminal.ReadPassword(int(syscall.Stdin));
-  fmt.Println("\nConnecting..");
-  if err != nil {
-    log.Fatal(err);
-  }
+    fmt.Print("Password: ");
+    bytePassword, err := terminal.ReadPassword(int(syscall.Stdin));
+    fmt.Println("\nConnecting..");
+    if err != nil {
+      log.Fatal(err);
+    }
 
-  // Extracted Credentials
-  password := string(bytePassword);
-  username := strings.Split(inputUsername, "\n")[0];
+    // Extracted Credentials
+    password := string(bytePassword);
+    username := strings.Split(inputUsername, "\n")[0];
 
-  resp, err := http.PostForm("https://10.100.56.55:8090/login.xml",
-    url.Values{
-      "a": {"1524343263066"},
-      "mode": {"191"},
-      "username": {username},
-      "password": {password},
-      "producttype": {"0"}});
-  if err != nil {
-    log.Fatal(err)
-  }
+    resp, err := http.PostForm("https://10.100.56.55:8090/login.xml",
+      url.Values{
+        "a": {"1524343263066"},
+        "mode": {"191"},
+        "username": {username},
+        "password": {password},
+        "producttype": {"0"}});
+    if err != nil {
+      log.Fatal(err)
+    }
 
-  defer resp.Body.Close();
+    defer resp.Body.Close();
 
-  XMLBytes, err := ioutil.ReadAll(resp.Body);
+    XMLBytes, err := ioutil.ReadAll(resp.Body);
 
-  if err != nil {
-    log.Fatal(err)
-  }
+    if err != nil {
+      log.Fatal(err)
+    }
 
-  // Checking Status
-  if ReLogin.FindSubmatch(XMLBytes) != nil {
-    fmt.Println("Succeefully Logged In.");
-  } else if ReMaxLimit.FindSubmatch(XMLBytes) != nil {
-    fmt.Println("Maximum Login Limit Reach.");
-    goto getCredentials;
-  } else if ReInvalidCred.FindSubmatch(XMLBytes) != nil {
-    fmt.Println("Invalid Credentials.");
-    goto getCredentials;
+    // Checking Status
+    if ReLogin.FindSubmatch(XMLBytes) != nil {
+      fmt.Println("Succeefully Logged In.");
+    } else if ReMaxLimit.FindSubmatch(XMLBytes) != nil {
+      fmt.Println("Maximum Login Limit Reach.");
+      goto getCredentials;
+    } else if ReInvalidCred.FindSubmatch(XMLBytes) != nil {
+      fmt.Println("Invalid Credentials.");
+      goto getCredentials;
+    } else {
+      fmt.Println("Data Exceed.");
+      goto getCredentials;
+    }
   } else {
-    fmt.Println("Data Exceed.");
-    goto getCredentials;
+    help();
+    os.Exit(0);
   }
 }
